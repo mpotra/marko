a03badbae08a569e972b16a007ecc756fdbcd053
Mihai Potra <mike@mpotra.com>
2023-05-09 16:35:04 +0300

    test: Fix homepage test

--- test/marko_web/controllers/page_controller_test.exs
+++ test/marko_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MarkoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Select a page"
   end
 end
