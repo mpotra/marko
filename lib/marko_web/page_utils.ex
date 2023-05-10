dd2533940cea5483133851a76489b09d12c1f9ba
Mihai Potra <mike@mpotra.com>
2023-05-10 09:31:35 +0300

    lib: Add PageUtils module

---
+++ lib/marko_web/page_utils.ex
@@ -0,0 +1,89 @@
+defmodule MarkoWeb.PageUtils do
+  @moduledoc """
+  Module that provides helper functions for
+  handline PageView records in a LiveView
+  """
+  use MarkoWeb, :live_util
+
+  alias Marko.{PageView, PageViews}
+  alias Phoenix.LiveView.Socket
+
+  @spec get_engagement_time(socket :: Socket.t()) :: pos_integer()
+  def get_engagement_time(%{assigns: %{pageview: %PageView{engagement_time: engagement_time}}}) do
+    engagement_time
+  end
+
+  def get_engagement_time(_) do
+    0
+  end
+
+  @spec end_pageview(socket :: Socket.t()) :: Socket.t()
+  def end_pageview(
+        %{
+          assigns: %{
+            pageview: %PageView{} = pageview
+          }
+        } = socket
+      ) do
+    pageview =
+      pageview
+      |> PageView.pause()
+      |> PageViews.register!()
+
+    socket
+    |> assign(:pageview, %{pageview | paused_at: DateTime.utc_now()})
+  end
+
+  def end_pageview(socket) do
+    socket
+  end
+
+  @spec assign_new_pageview(socket :: Socket.t(), module :: module()) :: Socket.t()
+  def assign_new_pageview(%{assigns: %{session_id: session_id, page: page}} = socket, module)
+      when not is_nil(session_id) do
+    pageview = %PageView{
+      session_id: session_id,
+      view: module,
+      page: page,
+      engagement_time: 0,
+      paused_at: DateTime.utc_now(),
+      updated_at: DateTime.utc_now()
+    }
+
+    socket |> assign(:pageview, pageview)
+  end
+
+  def assign_new_pageview(socket, _module) do
+    socket
+  end
+
+  @spec pause_pageview(socket :: Socket.t()) :: Socket.t()
+  def pause_pageview(
+        %{
+          assigns: %{
+            pageview: %PageView{} = pageview
+          }
+        } = socket
+      ) do
+    socket |> assign(:pageview, PageView.pause(pageview))
+  end
+
+  def pause_pageview(socket) do
+    socket
+  end
+
+  @spec resume_pageview(socket :: Socket.t()) :: Socket.t()
+  def resume_pageview(
+        %{
+          assigns: %{
+            pageview: %PageView{} = pageview
+          }
+        } = socket
+      ) do
+    socket |> assign(:pageview, PageView.resume(pageview))
+  end
+
+  def resume_pageview(socket) do
+    socket
+  end
+end
