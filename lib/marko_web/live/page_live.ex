29a8ca325b37f2ac5c9885b404ef3521b0331027
Mihai Potra <mike@mpotra.com>
2023-05-10 09:32:41 +0300

    lib: Integrate PageUtils

--- lib/marko_web/live/page_live.ex
+++ lib/marko_web/live/page_live.ex
@@ -5,6 +5,8 @@ defmodule MarkoWeb.PageLive do
 
   use MarkoWeb, :live_view
 
+  alias MarkoWeb.PageUtils
+
   @impl true
   def render(assigns) do
     ~H"""
@@ -46,25 +48,53 @@ defmodule MarkoWeb.PageLive do
   end
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, %{"session_id" => session_id}, socket) do
+    {:ok,
+     socket
+     |> assign(:session_id, session_id)}
   end
 
   @impl true
   def handle_params(_params, _uri, socket) do
-    socket =
-      case socket.assigns.live_action do
-        :page_a ->
-          socket
-          |> assign(:page_title, "Page A")
-          |> assign(:page, :page_a)
+    next_page = socket.assigns.live_action
 
-        :page_b ->
-          socket
-          |> assign(:page_title, "Page B")
-          |> assign(:page, :page_b)
+    socket =
+      if Map.get(socket.assigns, :page) != next_page do
+        socket = PageUtils.end_pageview(socket)
+
+        next_page
+        |> case do
+          :page_a ->
+            socket
+            |> assign(:page_title, "Page A")
+            |> assign(:page, :page_a)
+
+          :page_b ->
+            socket
+            |> assign(:page_title, "Page B")
+            |> assign(:page, :page_b)
+        end
+        |> PageUtils.assign_new_pageview(__MODULE__)
+      else
+        socket
       end
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("page-resume", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.resume_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
+  end
+
+  def handle_event("page-pause", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.pause_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
+  end
+
+  def handle_event("page-unload", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.end_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
   end
 end
--- lib/marko_web/live/tabbed_page_live.ex
+++ lib/marko_web/live/tabbed_page_live.ex
@@ -5,12 +5,13 @@ defmodule MarkoWeb.TabbedPageLive do
 
   use MarkoWeb, :live_view
   alias MarkoWeb.LiveTabs
+  alias MarkoWeb.PageUtils
 
   @impl true
   def render(assigns) do
     ~H"""
     <section>
-      <.live_component module={LiveTabs} id="tabs" select_tab={@tab}>
+      <.live_component module={LiveTabs} id="tabs" select_tab={@page}>
         <:tab id={:tab_1} path={~p"/page_c/tab_1"} name="Tab 1">
           <p>
             Go to&nbsp;
@@ -33,30 +34,59 @@ defmodule MarkoWeb.TabbedPageLive do
   end
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, %{"session_id" => session_id}, socket) do
+    {:ok,
+     socket
+     |> assign(:session_id, session_id)}
   end
 
   @impl true
   def handle_params(_params, _uri, socket) do
+    next_page = socket.assigns.live_action
+
     socket =
-      case socket.assigns.live_action do
-        :index ->
-          random_tab = Enum.random(["tab_1", "tab_2"])
-          random_tab_path = "/page_c/#{random_tab}"
-          push_navigate(socket, to: random_tab_path, replace: true)
+      if Map.get(socket.assigns, :page) != next_page do
+        socket = PageUtils.end_pageview(socket)
 
-        :tab_1 ->
-          socket
-          |> assign(:page_title, "Tab 1 - Page C")
-          |> assign(:tab, :tab_1)
+        next_page
+        |> case do
+          :index ->
+            random_tab = Enum.random(["tab_1", "tab_2"])
+            random_tab_path = "/page_c/#{random_tab}"
+            push_navigate(socket, to: random_tab_path, replace: true)
 
-        :tab_2 ->
-          socket
-          |> assign(:page_title, "Tab 2 - Page C")
-          |> assign(:tab, :tab_2)
+          :tab_1 ->
+            socket
+            |> assign(:page_title, "Tab 1 - Page C")
+            |> assign(:page, :tab_1)
+            |> PageUtils.assign_new_pageview(__MODULE__)
+
+          :tab_2 ->
+            socket
+            |> assign(:page_title, "Tab 2 - Page C")
+            |> assign(:page, :tab_2)
+            |> PageUtils.assign_new_pageview(__MODULE__)
+        end
+      else
+        socket
       end
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("page-resume", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.resume_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
+  end
+
+  def handle_event("page-pause", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.pause_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
+  end
+
+  def handle_event("page-unload", %{"page" => _page} = _params, socket) do
+    socket = PageUtils.end_pageview(socket)
+    {:reply, %{engagementTime: PageUtils.get_engagement_time(socket)}, socket}
   end
 end
